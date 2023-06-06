ef71eb1d9295d83e476095971c4b2b8987a16c4b
Dongja <jdh981118@naver.com>
2023-06-06 23:55:24 +0900

    feat: navbar 로고, cart 클릭시 라우팅 추가

--- shoppy/src/components/Navbar.jsx
+++ shoppy/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { FiShoppingBag, FiShoppingCart } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleClickCard = () => navigate('/carts');
+
   return (
     <div className="flex items-center justify-around py-2">
-      <div className="flex items-center text-orange-600 text-3xl cursor-pointer">
-        <FiShoppingBag className="mr-2" />
-        <p>Shoppy</p>
-      </div>
+      <Link to="/">
+        <div className="flex items-center text-orange-600 text-3xl cursor-pointer">
+          <FiShoppingBag className="mr-2" />
+          <p>Shoppy</p>
+        </div>
+      </Link>
       <div className="flex items-center">
-        <p className="text-lg mr-2 cursor-pointer">Products</p>
-        <FiShoppingCart className="text-xl mr-4 cursor-pointer" />
+        <Link to="/products">
+          <p className="text-lg mr-2 cursor-pointer">Products</p>
+        </Link>
+        <FiShoppingCart
+          className="text-xl mr-4 cursor-pointer"
+          onClick={handleClickCard}
+        />
         <button className="text-xl outline-none bg-red-400 text-zinc-100 p-2 font-bold">
           Login
         </button>
